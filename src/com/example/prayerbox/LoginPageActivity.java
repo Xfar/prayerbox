bb26e161f7fcc216c74f8d5dc6acd0a549260de9
Emmanuel Lee <eman.j.lee@gmail.com>
2013-11-14 21:33:46 -0500

    Fixed up layouts. Emman's first commit.

--- src/com/example/prayerbox/LoginPageActivity.java
+++ src/com/example/prayerbox/LoginPageActivity.java
@@ -21,73 +21,77 @@ import android.widget.ListView;
 
 public class LoginPageActivity extends ListActivity {
 	public ArrayList<String> your_array_list;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_page);
-      
-        new GetData().execute(""); 
-    }
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login_page);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.login_page, menu);
-        ActionBar bar = this.getActionBar();
-        bar.setTitle(R.string.prayer_list);
-        return true;
-    }
-    
-    @Override
-    protected void onListItemClick(ListView l, View v, int position, long id) {
-        Prayer item = (Prayer) getListAdapter().getItem(position);
-        String request = item.request;
-        String subject = item.subject;
-        Intent nextScreen = new Intent(getApplicationContext(), PrayerDetailsActivity.class);
-        
-        //Sending data to another Activity
-        nextScreen.putExtra("subject", subject);
-        nextScreen.putExtra("request", request);
+		new GetData().execute("");
+	}
 
-        startActivity(nextScreen);
-    }
-    
-    private class GetData extends AsyncTask<String, Void, String>{
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.login_page, menu);
+		ActionBar bar = this.getActionBar();
+		bar.setTitle(R.string.prayer_list);
+		return true;
+	}
+
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		Prayer item = (Prayer) getListAdapter().getItem(position);
+		String request = item.request;
+		String subject = item.subject;
+		Intent nextScreen = new Intent(getApplicationContext(),
+				PrayerDetailsActivity.class);
+
+		// Sending data to another Activity
+		nextScreen.putExtra("subject", subject);
+		nextScreen.putExtra("request", request);
+
+		startActivity(nextScreen);
+	}
+
+	private class GetData extends AsyncTask<String, Void, String> {
 		private String result;
-		private ProgressDialog Dialog = new ProgressDialog(LoginPageActivity.this);
+		private ProgressDialog Dialog = new ProgressDialog(
+				LoginPageActivity.this);
+
 		@Override
-		protected String doInBackground(String... params){
-		    HttpPost httpMethod = new HttpPost("http://www.uwccf.ca/prayerbox/api/prayerproxy.php");
-		    
-		    DefaultHttpClient client =  new DefaultHttpClient();
-		    result = null;
-		    try {
-		        HttpResponse response = client.execute(httpMethod);
-		
-		        HttpEntity entity = response.getEntity();
-		        result = EntityUtils.toString(entity);
-		        return result;
-		        } catch (Exception e) {
-		        e.printStackTrace();
-		    }
+		protected String doInBackground(String... params) {
+			HttpPost httpMethod = new HttpPost(
+					"http://www.uwccf.ca/prayerbox/api/prayerproxy.php");
+
+			DefaultHttpClient client = new DefaultHttpClient();
+			result = null;
+			try {
+				HttpResponse response = client.execute(httpMethod);
+
+				HttpEntity entity = response.getEntity();
+				result = EntityUtils.toString(entity);
+				return result;
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 			return null;
 		}
-    	
-	  @Override
-	    protected void onPreExecute()
-	    {
-	        Dialog.setMessage("Loading Requests...");
-	        Dialog.show();
-	    }
-	    	  
-	    @Override
-	    protected void onPostExecute(String result) {
-	        PrayerParser pray_parser = new PrayerParser(result);
-	        ArrayList<Prayer> prayer_list = pray_parser.parse();
-	        PrayerAdapter prayerAdapter = new PrayerAdapter(LoginPageActivity.this,prayer_list);
-	        setListAdapter(prayerAdapter);
-	        Dialog.dismiss();
-	    }
-	 }
+
+		@Override
+		protected void onPreExecute() {
+			Dialog.setMessage("Loading prayer requests...");
+			Dialog.show();
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			PrayerParser pray_parser = new PrayerParser(result);
+			ArrayList<Prayer> prayer_list = pray_parser.parse();
+			PrayerAdapter prayerAdapter = new PrayerAdapter(
+					LoginPageActivity.this, prayer_list);
+			setListAdapter(prayerAdapter);
+			Dialog.dismiss();
+		}
+	}
 }
--- src/com/example/prayerbox/PrayerDetailsActivity.java
+++ src/com/example/prayerbox/PrayerDetailsActivity.java
@@ -1,27 +1,43 @@
 package com.example.prayerbox;
 
 import android.app.Activity;
-import android.text.method.ScrollingMovementMethod;
-import android.view.MotionEvent;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.text.method.ScrollingMovementMethod;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class PrayerDetailsActivity extends Activity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.pray_detail);
-        TextView txtRequest = (TextView) findViewById(R.id.prayer_request);
-        txtRequest.setMovementMethod(new ScrollingMovementMethod());
-        
-        Intent i = getIntent();
-        // Receiving the Data
-        String subject = i.getStringExtra("subject");
-        String request = i.getStringExtra("request");
- 
-        // Displaying Received data
-        setTitle(subject);
-        txtRequest.setText(request);
-    }
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.pray_detail);
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+		getActionBar().setDisplayShowTitleEnabled(true);
+
+		Intent i = getIntent();
+		// Receiving the Data
+		String subject = i.getStringExtra("subject");
+		String request = i.getStringExtra("request");
+
+		TextView txtRequest = (TextView) findViewById(R.id.prayer_request);
+
+		// Displaying Received data
+		setTitle(subject);
+		txtRequest.setText(request);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		// Respond to the action bar's Up/Home button
+		case android.R.id.home:
+			NavUtils.navigateUpFromSameTask(this);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
 }
